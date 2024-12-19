c71c63a82dd054f8d75699ccd202c0de72433d0c
Dellenoam <d.spiridonov.0256@gmail.com>
2024-12-19 22:05:00 +0500

    🔍 Reset config options to default ones

--- config/user_config.py
+++ config/user_config.py
@@ -2,14 +2,14 @@
 gas_unit_price = 100  # price per gas unit
 max_gas_amount = 1000  # max gas amount per transaction you ready to pay
 delay_between_transactions = [2, 4]  # delay between transactions (in seconds)
-wait_for_transaction = False  # wait for transaction completion
+wait_for_transaction = True  # wait for transaction completion
 transaction_wait_in_seconds = 60  # wait time for transaction completion (in seconds)
 retries = 3  # number of retries on transaction failure
 
 # Transfer Settings
 use_all_balance = False  # transfer all balance
-transfer_amount = [1, 2]  # transfer amount in APT
+transfer_amount = [1, 2]  # transfer amount in APT. if use_all_balance is True, then this value will be ignored
 
 # General Settings
-play_intro = False  # play intro on start of the script
-debug_mode = True  # use testnet or mainnet
+play_intro = True  # play intro on start of the script
+debug_mode = False  # use testnet or mainnet
